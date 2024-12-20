68e31bb09fc9738bb4f52e04647f2c32e42eb38b
vsechaud <victor.sechaud@ens-lyon.fr>
2024-12-20 11:29:37 +0100

    resume trainer

--- deepinv/training/trainer.py
+++ deepinv/training/trainer.py
@@ -1177,7 +1177,6 @@ class Trainer:
         :returns: dict of metrics results with means and stds.
         """
         self.compare_no_learning = compare_no_learning
-        save_path_out = self.save_path
         self.setup_train(train=False)
 
         self.save_folder_im = save_path
@@ -1231,7 +1230,6 @@ class Trainer:
             if self.verbose:
                 print(f"{name}: {l.avg:.3f} +- {l.std:.3f}")
 
-        torch.save(out, save_path_out + "/test_results.pth")
         return out
 
 
