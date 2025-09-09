b981bfccf7255a35e39b83f91252a014019b58f5
Limonet Julie <jlimonet@pxe.cbp.ens-lyon.fr>
2025-09-09 15:11:36 +0200

    debugging bootstrap

--- deepinv/models/bootstrap.py
+++ deepinv/models/bootstrap.py
@@ -33,8 +33,8 @@ class Bootstrap(Reconstructor):
         If ``T.n_trans`` is different from ``MC``, it will be overridden to match ``MC``.
     """
 
-    def __init__(self, img_size, model, physics, T=Identity(), MC=100, **kwargs):
-        super(Bootstrap, self).__init__(**kwargs)
+    def __init__(self, img_size, model, physics, T=Identity(), MC=100, device='cpu'):
+        super(Bootstrap, self).__init__()
         self.model = model
         self.T = T
         self.MC = MC
@@ -43,6 +43,7 @@ class Bootstrap(Reconstructor):
         T.n_trans = MC
         self.img_size = img_size
         self.physics = physics
+        self.device = device  
 
 
     def forward(self, y, physics, **kwargs):
