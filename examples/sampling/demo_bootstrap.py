b25c0ed93b0637824018881648c62322ac2e1f23
vsechaud <victor.sechaud@ens-lyon.fr>
2025-09-10 09:55:13 +0200

    update demo

--- examples/sampling/demo_bootstrap.py
+++ examples/sampling/demo_bootstrap.py
@@ -19,8 +19,8 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize((256, 256)),  # converts PIL Image to torch.Tensor
 ])
-
-train_dataset = dinv.datasets.DIV2K(root="./data/DIV2K", download=False, mode='train', transform=transform)
+print("1")
+# train_dataset = dinv.datasets.DIV2K(root="./data/DIV2K", download=False, mode='train', transform=transform)
 test_dataset = dinv.datasets.DIV2K(root="./data/DIV2K", download=False, mode='val', transform=transform)
 
 img_size = (3, 256, 256)
@@ -34,9 +34,9 @@ physics = dinv.physics.Inpainting(
 )
 
 num_workers = 4 if torch.cuda.is_available() else 0
-
+print("2")
 deepinv_datasets_path = dinv.datasets.generate_dataset(
-    train_dataset=train_dataset,
+    train_dataset=None,
     test_dataset=test_dataset,
     physics=physics,
     device=device,
@@ -65,10 +65,10 @@ model.eval()
 from deepinv.sampling.uncertainty_quantification import UQ
 from deepinv.models.bootstrap import Bootstrap
 
-bootstrap_model = Bootstrap(model=model, img_size=img_size, physics=physics, T=dinv.transform.Shift(n_trans=100), MC=100, device=device)
+bootstrap_model = Bootstrap(model=model, img_size=img_size, physics=physics, T=dinv.transform.Shift(), MC=2, device=device)
 
 # %%
-
-uq = UQ(img_size=img_size, dataloader=dataloader, model=bootstrap_model, metric=None) # METRIC TO REMOVE !!!!!
+print("3")
+uq = UQ(img_size=img_size, dataloader=dataloader, model=bootstrap_model)
 uq.compute_estimateMSE()
 uq.plot_coverage()
